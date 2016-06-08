66f6055c96007916c26a83f5c4c3affd7c7f5828
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2016-06-08 23:16:00 +0300

    Restructured files Added menu with logo Added singleton

---
+++ Assets/Scripts/MenuButtonControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonControl : MonoBehaviour {
+
+    public GameObject loadingImage;
+
+	// Use this for initialization
+	void Awake () {
+	
+	}
+	
+    public void LoadLevel() {
+        loadingImage.SetActive (true);
+        SceneManager.LoadSceneAsync (1);
+    }
+}
