f9ba1885fc43ca0d7f2dda970dce1cd770a3c079
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2016-06-14 00:12:16 +0300

    Added intro skipping fucntionality

--- Assets/Scripts/AnimatedShipController.cs
+++ Assets/Scripts/AnimatedShipController.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.Runtime.CompilerServices;
 
 public class AnimatedShipController : MonoBehaviour {
 
+
+    private Animator flyInController;
+    private bool skippedIntro = false;
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+        flyInController = GetComponent<Animator> ();
 	}
 	
     public void LoadMenu() {
+        if (!skippedIntro) {
+            skippedIntro = true;
+        }
+        GetComponent<AudioSource> ().enabled = false;
         SceneManager.LoadScene (1, LoadSceneMode.Additive);
+    }
+
+    void Update() {
+
+        if (!skippedIntro) {
+            if (Input.anyKey) {
+                skippedIntro = true;
+                flyInController.SetBool ("Pressed", true);
+
+                LoadMenu ();
+            }
+        }
     }
 }
