4db7f13aaabf3243d2cf22f6a2bc15b7ed05c1a9
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2016-09-03 00:51:04 +0300

    Creating final game storyline scenes

---
+++ Assets/Scripts/GameFinishController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameFinishController : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		//TODO: configure input file for ending
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//TODO: scroll through text for player
+		//TODO eventually move on to credits game scene
+	}
+}
